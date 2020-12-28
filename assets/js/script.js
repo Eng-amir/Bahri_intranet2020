02ae21dc19473c28766ce194197792903d11d1b3
Amir abdel-aziz <amir.alshiekh82@gmail.com>
2020-12-28 12:51:00 +0200

    First commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,19 @@ $(document).ready(function () {
         loop: false,
         rewind: true,
         responsiveClass: true,
+        autoplay: true,
         animateOut: 'fadeOut',
         animateIn: 'fadeIn',
-        autoplay: true,
         autoplayTimeout: 5000,
+        smartSpeed: 800,
+        touchDrag:true,
+        pullDrag:true,
+        mouseDrag:true,
         margin: 0,
         nav: true,
         dots: false,
         autoHeight: false,
         autoHeightClass: 'owl-height',
-        mouseDrag: false,
         autoplayHoverPause: true,
         items: 1,
         navText: [
@@ -53,15 +56,22 @@ $(document).ready(function () {
         rewind: true,
         responsiveClass: true,
         animateOut: 'slideOutLeft',
-        animateIn: 'slideInUp',
+        animateIn: 'slideInRight',
         autoplay: false,
         autoplayTimeout: 5000,
+        touchDrag:true,
+        pullDrag:true,
+        mouseDrag:true,
+        // freeDrag:true,
+        smartSpeed: 500,
+        // autoWidth: true,
+        slideBy:1,
+        autoplaySpeed: 500,
         margin: 0,
         nav: false,
         dots: true,
         autoHeight: false,
         autoHeightClass: 'owl-height',
-        mouseDrag: false,
         autoplayHoverPause: true,
         items: 1
     })
@@ -71,16 +81,23 @@ $(document).ready(function () {
         loop: false,
         rewind: true,
         responsiveClass: true,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
+        animateOut: 'slideOutLeft',
+        animateIn: 'slideInRight',
         autoplay: false,
         autoplayTimeout: 5000,
+        touchDrag:true,
+        pullDrag:true,
+        mouseDrag:true,
+        // freeDrag:true,
+        smartSpeed: 500,
+        // autoWidth: true,
+        slideBy:1,
+        autoplaySpeed: 500,
         margin: 0,
         nav: false,
         dots: true,
         autoHeight: false,
         autoHeightClass: 'owl-height',
-        mouseDrag: false,
         autoplayHoverPause: true,
         items: 1
     })
@@ -89,16 +106,23 @@ $(document).ready(function () {
         loop: false,
         rewind: true,
         responsiveClass: true,
-        animateOut: 'slideOutDown',
-        animateIn: 'flipInX',
+        animateOut: 'slideOutLeft',
+        animateIn: 'slideInRight',
         autoplay: false,
         autoplayTimeout: 5000,
+        touchDrag:true,
+        pullDrag:true,
+        mouseDrag:true,
+        // freeDrag:true,
+        smartSpeed: 500,
+        // autoWidth: true,
+        slideBy:1,
+        autoplaySpeed: 500,
         margin: 0,
         nav: false,
         dots: true,
         autoHeight: false,
         autoHeightClass: 'owl-height',
-        mouseDrag: false,
         autoplayHoverPause: true,
         items: 1
     })
@@ -108,16 +132,23 @@ $(document).ready(function () {
         loop: false,
         rewind: true,
         responsiveClass: true,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
+        animateOut: 'slideOutLeft',
+        animateIn: 'slideInRight',
         autoplay: false,
         autoplayTimeout: 5000,
+        touchDrag:true,
+        pullDrag:true,
+        mouseDrag:true,
+        // freeDrag:true,
+        smartSpeed: 500,
+        // autoWidth: true,
+        slideBy:1,
+        autoplaySpeed: 500,
         margin: 0,
         nav: false,
         dots: true,
         autoHeight: false,
         autoHeightClass: 'owl-height',
-        mouseDrag: false,
         autoplayHoverPause: true,
         items: 1
     })
@@ -127,21 +158,79 @@ $(document).ready(function () {
         loop: false,
         rewind: true,
         responsiveClass: true,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
+        animateOut: 'slideOutLeft',
+        animateIn: 'slideInRight',
         autoplay: false,
         autoplayTimeout: 5000,
+        touchDrag:true,
+        pullDrag:true,
+        mouseDrag:true,
+        // freeDrag:true,
+        smartSpeed: 500,
+        // autoWidth: true,
+        slideBy:1,
+        autoplaySpeed: 500,
         margin: 0,
         nav: false,
         dots: true,
         autoHeight: false,
         autoHeightClass: 'owl-height',
-        mouseDrag: false,
         autoplayHoverPause: true,
         items: 1
     })
+    $('.social-carousel.owl-carousel').owlCarousel({
+        loop: false,
+        rewind: true,
+        responsiveClass: true,
+        animateOut: 'slideOutLeft',
+        animateIn: 'slideInRight',
+        autoplay: false,
+        autoplayTimeout: 5000,
+        touchDrag:true,
+        pullDrag:true,
+        mouseDrag:true,
+        // freeDrag:true,
+        smartSpeed: 500,
+        // autoWidth: true,
+        slideBy:1,
+        autoplaySpeed: 500,
+        margin: 0,
+        nav: false,
+        dots: true,
+        autoHeight: false,
+        autoHeightClass: 'owl-height',
+        autoplayHoverPause: true,
+        items: 1,
+        dotsData: true,
+        addClassActive:true,
+       // afterAction: afterActionFunc
+    })
+
+    // function afterActionFunc(el){
+    //     //remove class active
+    //     this.$owlItems.removeClass('active')
+    //
+    //     //add class active //owl internal $ object containing items
+    //     this.$owlItems.eq(this.currentItem + 1)
+    //         .addClass('active')
+    // }
+
+    document.getElementById('next-slide').addEventListener('click', function(){
+        $('.social-carousel').trigger('next.owl.carousel');
+    });
+
+    document.getElementById('prev-slide').addEventListener('click', function(){
+        $('.social-carousel').trigger('prev.owl.carousel');
+    });
+
+
+    $(".panel-tab").click(function(event) {
+        event.preventDefault();
+        $("div.overlay").fadeToggle("fast");
+    });
 
     //slide panel
+    // $("div.overlay").hide();
     $('.panel-tab').click(function(){
         $('.menu-hide').toggleClass('show');
         $('.panel-tab .btn-secondary').addClass('hide-btn');
@@ -150,8 +239,43 @@ $(document).ready(function () {
     $('.xclose').click(function(){
         $('.menu-hide').removeClass('show');
         $('.panel-tab .btn-secondary').removeClass('hide-btn');
+        $("div.overlay").fadeOut(300, 'linear');
         $('.menu-tab').removeClass('active');
     });
+
+    $(function(){
+        $('body').mouseup(function(e){
+            var container = $(".menu-hide");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+                $('.panel-tab .btn-secondary').removeClass('hide-btn');
+                $('.menu-hide').removeClass('show');
+                $("div.overlay").fadeOut(300, 'linear');
+            }
+        });
+    });
+
+    //search
+    $(".search").click(function(){
+        $(".input").toggleClass("active").focus;
+        $(this).toggleClass("animate");
+        $('.search-close').toggleClass('visible');
+    });
+
+    $("#clear").click(function(){
+        $(".input").val("");
+    });
+
+    $(function(){
+        $('body').mouseup(function(e){
+            let topSearch = $(".input");
+            let searchClose = $('.search-close')
+            if (!topSearch.is(e.target) && topSearch.has(e.target).length === 0) {
+                topSearch.removeClass("active");
+                searchClose.removeClass('visible');
+            }
+        });
+    });
+
     $(function() {
         App.init();
     });
@@ -183,7 +307,7 @@ var App = {
     }
 }
 
-var myModalEl = document.getElementById('exampleModal')
+var myModalEl = document.getElementById('empModal')
 var modal = bootstrap.Modal.getInstance(myModalEl)
 var popover = new bootstrap.Popover(document.querySelector('.popo'), {
     container: 'body'
@@ -192,9 +316,21 @@ $(document).ready(function () {
     $('.toast').toast('show');
 });
 
+//Main Menu Link
+jQuery(".wsmenu>.wsmenu-list>li.tohide").on('click', function () {
+    jQuery(this).toggleClass("wsclickopen").siblings(this).removeClass("wsclickopen");
+    jQuery(".wsmenu>.wsmenu-list>li.tohide>ul.extra").removeClass('wsclickopen-sub');
+});
+
+jQuery(document).on("click", function (event) {
+    let $trigger = $(".wsmenu-list");
+    if ($trigger !== event.target && !$trigger.has(event.target).length) {
+        jQuery(".wsmenu>.wsmenu-list>li.tohide").removeClass("wsclickopen");
+    }
+});
+
 $(function() {
     $('.card').matchHeight();
 });
-// popover.show()
-//Popup
+
 
